52a1ba4b8788b54d100b0594cab57841996f5a1f
Mxgalodon <116174094+Mxgalodon@users.noreply.github.com>
2023-04-11 23:39:51 +0200

    update

---
+++ Assets/Systems/SmartObjects/Scripts/SimpleAI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Navigation_UnityNavMesh))]
+public class SimpleAI : MonoBehaviour
+{
+    [SerializeField] protected float PickInteractionInterval = 2f;
+    [SerializeField] protected Navigation_UnityNavMesh Navigation;
+
+    protected BaseInteraction CurrentInteraction = null;
+    protected bool StartedPerforming = false;   
+
+    protected float TimeUntilNextInteractionPicked = -1f;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentInteraction != null && Navigation.IsAtDestination)
+        {
+            if (Navigation.IsAtDestination && !StartedPerforming)
+            {
+                StartedPerforming = true;
+                CurrentInteraction.Perform(this, OnInteractionFinished);
+            }
+        }
+        else
+        {
+            TimeUntilNextInteractionPicked -= Time.deltaTime;
+            // czas na wybranie interakcji
+            if (TimeUntilNextInteractionPicked <= 1)
+            {
+                TimeUntilNextInteractionPicked = PickInteractionInterval;
+                PickRandomInteraction();
+            }
+        }
+    }
+    void OnInteractionFinished(BaseInteraction interaction)
+    {
+        interaction.UnlockInterraction();
+        CurrentInteraction = null;
+        Debug.Log($"Finished{interaction.DisplayName}");
+    }
+
+    void PickRandomInteraction()
+    {
+        //losowy przedmiot
+        int objectIndex = Random.Range(0, SmartObjectManager.Instance.RegisteredObjects.Count);
+        var selectedObject = SmartObjectManager.Instance.RegisteredObjects[objectIndex];
+        // losowa interakcja
+        int interactionIndex = Random.Range(0, selectedObject.Interactions.Count);
+        var selectedInteraction = selectedObject.Interactions[interactionIndex];
+        
+        //czy mozna wykonac interakcje?
+        if(selectedInteraction.CanPerform())
+        {
+            CurrentInteraction = selectedInteraction;
+            CurrentInteraction.LockInteraction();
+            StartedPerforming = false;
+            //poruszenie sie do celu
+            Debug.Log(Navigation);
+            Debug.Log(CurrentInteraction);
+            if (!Navigation.SetDestination(selectedObject.InteractionPoint))
+                
+            {
+                Debug.LogError($"Could not move to {selectedObject.name}");
+                CurrentInteraction = null;
+            }
+            else
+                Debug.Log($"Going to {CurrentInteraction.DisplayName} at {selectedObject.DisplayName}");
+        }
+    }
+}
---
+++ Assets/Systems/SmartObjects/Scripts/SimpleInteraction.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleInteraction : BaseInteraction
+{
+    protected class PerformerInfo
+    {
+        public float ElapsedTime;
+        public UnityAction<BaseInteraction> OnCompleted;
+    }
+    [SerializeField] protected int MaxSimultaneousUsers = 1;
+
+    protected int NumCurrentUsers = 0;
+    protected List<PerformerInfo> CurrentPerformers = new List<PerformerInfo>();
+
+    public override bool CanPerform()
+    {
+        return NumCurrentUsers < MaxSimultaneousUsers;
+    }
+
+    public override void LockInteraction()
+    {
+        ++NumCurrentUsers;
+
+        if (NumCurrentUsers > MaxSimultaneousUsers)
+            Debug.LogError($"Too many users have locked this interaction {_DisplayName}");
+    }
+
+    public override void Perform(MonoBehaviour performer, UnityAction<BaseInteraction> onCompleted)
+    {
+        if (NumCurrentUsers <= 0)
+        {
+            Debug.LogError($"Trying to perform an action when there are no users {_DisplayName}");
+            return;
+        }
+        //sprawdzenie typu interakcji
+        if (InteractionType == EInteractionType.Instantaneous)
+        {
+            if (onCompleted != null)
+                onCompleted.Invoke(this);
+        }
+        else if (InteractionType == EInteractionType.OverTime)
+        {
+            CurrentPerformers.Add(new PerformerInfo() {ElapsedTime = 0, OnCompleted = onCompleted });
+        }
+    }
+
+    public override void UnlockInterraction()
+    {
+        if (NumCurrentUsers <= 0)
+            Debug.LogError($"Trying to unlock an already unlocked interaction {_DisplayName}");
+
+        --NumCurrentUsers;
+    }
+    protected virtual void Update()
+    {
+        //update any current performers
+        for (int index = CurrentPerformers.Count - 1; index >= 0; index--)
+        {
+            PerformerInfo performer = CurrentPerformers[index];
+
+            performer.ElapsedTime += Time.deltaTime;
+        //skoñczona interakcja?
+            if(performer.ElapsedTime >= _Duration)
+            {
+                performer.OnCompleted.Invoke(this);
+                CurrentPerformers.RemoveAt(index);
+            }
+        }
+    }
+}
